8de2665f12d3f3f582b7e468c721eb229d8a98fb
Franklin Silva <franklinsilva@outlook.com>
2018-12-27 23:59:42 -0300

    v.15

--- js/script.js
+++ js/script.js
@@ -23,6 +23,74 @@ function desgraca() {
 }
 
 toogleModal = function() {
-    var el = document.querySelector('.simple-modal');
+    var el = document.querySelector('.simple-modal-programacao');
     el.classList.toggle('displayNone');
 }
+
+var modalHotelContent = [
+    {
+        name: "Pestana Convento do Carmo",
+        href: "https://www.pestana.com/br/hotel/pestana-convento-do-carmo?gclid=CjwKCAiA0ajgBRA4EiwA9gFOR2c4flyNB4UVIevTvzh25Dyzhll23NdOjxp43bu8nLAW1-sfq7wP8xoCk-QQAvD_BwE&gclsrc=aw.ds",
+        img: "img/modal/300ppi/pestana.png"
+    },
+    {
+        name: "Hotel Monte Pascoal",
+        href: "www.montepascoalpraiahotel.com.br",
+        img: "img/modal/300ppi/pascoal.png"
+    },
+    {
+        name: "Hotel Sol Express",
+        href: "www.solexpress.com.br",
+        img: "img/modal/300ppi/sol.png"
+    },
+    {
+        name: "Sol Victoria Marina",
+        href: "www.solexpress.com.br/hoteis-e.../sol-victoria-marina/index.html",
+        img: "img/modal/300ppi/marina.png"
+    },
+    {
+        name: "Grande Hotel Barra",
+        href: "https://www.grandehoteldabarra.com.br",
+        img: "img/modal/300ppi/grande.png"
+    },
+    {
+        name: "Pousada Colonial Chile",
+        href: "https://www.colonialchilehotel.com.br",
+        img: "img/modal/300ppi/colonial.png"
+    },
+    {
+        name: "Hostel Hospeda Salvador",
+        href: "https://www.hospedasalvador.com.br",
+        img: "img/modal/300ppi/hostel.png"
+    },
+    {
+        name: "Pousada Suites do Pelô",
+        href: "https://www.suitesdopelo.com",
+        img: "img/modal/300ppi/suitesPelo.png"
+    },
+    {
+        name: "Hotel Porto da Barra",
+        href: "https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwiYoKDxmY_fAhUL5HcKHYp-DI8YABABGgJlZg&ei=fS4LXI2vK5uD1fAPiYSqyA8&ohost=www.google.com.br&cid=CAESEeD2MZpmvcVqQ9MuaA0i87jU&sig=AOD64_0IVvMS-JIQWL7rD9pgvTsKbLatZQ&q=&sqi=2&ved=2ahUKEwjN4ZbxmY_fAhWbQRUIHQmCCvkQ0Qx6BAgaEAE&adurl=",
+        img: "img/modal/300ppi/hpBarra.png"
+    },
+];
+
+toogleModalHotel = function() {
+    var el = document.querySelector('.simple-modal-hotel');
+    el.classList.toggle('displayNone'); 
+    
+};
+
+populateModal = function(index) {
+    var titleModal = document.querySelector('.simple-modal-hotel--title');
+    titleModal.innerHTML = modalHotelContent[index].name;
+
+    var imgModal = document.querySelector('.modal-hotel--img');
+    imgModal.src = modalHotelContent[index].img;
+
+    var hrefModal = document.querySelector('.modal-hotel--href');
+    hrefModal.href = modalHotelContent[index].href;
+
+
+    toogleModalHotel();
+};
